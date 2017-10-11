df8c695d2f8872d1863681270be14071774a4c41
adamsoeur <adamsoeur@yahoo.com>
2017-10-10 23:06:05 -0500

    Final Upload
    Final Commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '78KnIUkgfTMDklgsrjNsy4S9q',
+  consumer_secret: 'IGC7MADUsLBLhoLEc8ULG9QR4R8e0NsKRzNY2zECHJXRro4lSh',
+  access_token_key: '917950804652449792-mpAZ5XsAY6Lic9kUIzop6b1RuBlkfa8',
+  access_token_secret: 'bHHd5peaD07PsaGJHn0Y10Bv2AsR2GPLpz5aSYA3MheQG',
+}
---
+++ liri.js
@@ -0,0 +1,103 @@
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var spotify = require("spotify");
+var omdb = require("omdb");
+var fs = require("fs");
+
+function getMyTweets() {
+	var client = new Twitter(keys.twitterKeys);
+
+	client.get("statuses/user_timeline", "AdamSoeur1&count=20", function(error, tweets, response) {
+		
+		for (var i = 0; i < tweets.length; i++) {
+			console.log(tweets[i].created_at);
+			console.log(tweets[i].text);
+			console.log("--------------------------------------");
+		}
+
+	})
+}
+
+function spotifySong(song) {
+	spotify.search({type: "track", query: song}, function(error, data) {
+		if (error) {
+			console.log("Error occured: " + error);
+		}
+		var artists = data.tracks.items[0].album.artists;
+
+		for (var i = 0; i < artists.length; i++) {
+			console.log("Artist: " + artists[i].name);
+		}
+		
+		console.log("Track name: " + data.tracks.items[0].name);
+		console.log("Preview URL: " + data.tracks.items[0].preview_url);
+		console.log("Album: " + data.tracks.items[0].album.name);
+	})
+}
+
+function omdbMovie(movie) {
+	omdb.get({title: movie}, {tomatoes: true}, function(error, data) {
+
+		console.log("Title: " + data.title);
+		console.log("Year: " + data.year);
+		console.log("IMDB Rating: " + data.imdb.rating);
+		console.log("Countries of production: " + data.countries.join(", "));
+		console.log("Language: " + data.language); 
+		console.log("Plot: " + data.plot);
+		console.log("Actors: " + data.actors.join(", "));
+		if (data.tomato) {
+			console.log("Rotten Tomatoes Rating: " + data.tomato.rating);
+			console.log("Rotten Tomatoes URL: " + data.tomato.url);
+		}
+	})
+}
+
+function doWhatItSays() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		var args = data.split(",");
+		nodeArgs[2] = args[0];
+		query = args[1];
+		liri();
+	})
+}
+
+var nodeArgs = process.argv;
+var query = ""; 
+
+function liri() {
+	if (nodeArgs.length > 3) {
+		var queryArray = [];
+		for (var i = 3; i < nodeArgs.length; i++) {
+			queryArray.push(nodeArgs[i]);
+		}
+
+		query = queryArray.join(" ");
+	}
+
+
+	if (nodeArgs[2] === "my-tweets") {
+		getMyTweets();
+	}
+
+	if (nodeArgs[2] === "spotify-this-song") {
+		if (query === "") {
+			spotifySong("The Sign Ace of Base");
+		} else {
+			spotifySong(query);
+		}
+	}
+
+	if (nodeArgs[2] === "movie-this") {
+		if (query ==="") {
+			omdbMovie("Mr. Nobody");
+		} else {
+			omdbMovie(query);
+		}
+	}	
+}
+
+if (nodeArgs[2] === "do-what-it-says") {
+	doWhatItSays();
+}
+
+liri();
